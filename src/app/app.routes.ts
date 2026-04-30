fcff6113e115805848cc7b16aafcd4f1cd8782d2
Nikoleta Papalazarou <150321195+NikolPap@users.noreply.github.com>
2026-04-30 23:56:09 +0200

    Füge Routen für Rezeptgenerator und Präferenzen hinzu

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,11 @@
 import { Routes } from '@angular/router';
 import { LandingPage } from './pages/landing-page/landing-page';
+import { RecipeGenerator } from './pages/recipe-generator/recipe-generator';
+import { Preferences } from './pages/preferences/preferences';
 
 export const routes: Routes = [
-     { path: '', component: LandingPage}
+   { path: '', component: LandingPage },
+  { path: 'generate', component: RecipeGenerator},
+   { path: 'preferences', component: Preferences },
+  { path: '**', redirectTo: '' }
 ];
