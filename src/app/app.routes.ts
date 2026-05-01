a5a3726903e52abdb336b14aca8dd8aeb70f9f4b
Nikoleta Papalazarou <150321195+NikolPap@users.noreply.github.com>
2026-05-01 23:29:33 +0200

    Füge Route für Rezeptergebnisse zur Routing-Konfiguration hinzu

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { LandingPage } from './pages/landing-page/landing-page';
 import { RecipeGenerator } from './pages/recipe-generator/recipe-generator';
 import { Preferences } from './pages/preferences/preferences';
+import { RecipeResults } from './pages/recipe-results/recipe-results';
+
 
 export const routes: Routes = [
    { path: '', component: LandingPage },
   { path: 'generate', component: RecipeGenerator},
    { path: 'preferences', component: Preferences },
-  { path: '**', redirectTo: '' }
+     { path: 'results', component: RecipeResults}
 ];
