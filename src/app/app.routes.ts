3d29719698a55721e521e9d2d8c62991ee2e0c2c
Nikoleta Papalazarou <150321195+NikolPap@users.noreply.github.com>
2026-05-02 00:51:58 +0200

    Füge die Cookbook-Komponente zu den Routen hinzu

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { LandingPage } from './pages/landing-page/landing-page';
 import { RecipeGenerator } from './pages/recipe-generator/recipe-generator';
 import { Preferences } from './pages/preferences/preferences';
 import { RecipeResults } from './pages/recipe-results/recipe-results';
+import { Cookbook } from './pages/cookbook/cookbook';
 
 
 export const routes: Routes = [
    { path: '', component: LandingPage },
   { path: 'generate', component: RecipeGenerator},
    { path: 'preferences', component: Preferences },
+    { path: 'cookbook', component: Cookbook},
      { path: 'results', component: RecipeResults}
 ];
