a71237216e4a1a1aa23da12fe70c75b40e7627b8
Nikoleta Papalazarou <150321195+NikolPap@users.noreply.github.com>
2026-05-04 01:20:26 +0200

    Refaktoriere Rezeptdetail-Komponente zur Verwendung von Dependency Injection und verbessere die Rezeptdatenverwaltung

--- src/app/pages/recipe-detail/recipe-detail.ts
+++ src/app/pages/recipe-detail/recipe-detail.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, OnInit, inject, ChangeDetectorRef  } from '@angular/core';
+import { ActivatedRoute,RouterLink } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { RecipeService } from '../../services/recipe';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -9,40 +10,35 @@ import { CommonModule } from '@angular/common';
   styleUrl: './recipe-detail.scss',
 })
 export class RecipeDetail {
-    recipe = {
-    title: 'Pasta with spinach and cherry tomatoes',
-    time: '20min',
-    portions: 2,
-    chefsCount: 2,
-    likes: 66,
-    tags: ['Vegetarian', 'Quick'],
-    nutrition: { energy: '630 kcal', protein: '18g', fat: '24g', carbs: '58g' },
-    
-    myIngredients: [
-      { amount: '80g', name: 'Pasta noodles' },
-      { amount: '100g', name: 'Baby spinach' },
-      { amount: '150g', name: 'Cherry tomatoes' },
-      { amount: '1 piece', name: 'Egg' }
-    ],
-    extraIngredients: [
-      { amount: '40g', name: 'Parmesan cheese' },
-      { amount: '30ml', name: 'Olive oil' },
-      { amount: 'Herbs', name: '(dry basil, oregano, garlic)' }
-    ],
-
-    // Βήματα Εκτέλεσης (Το chefId καθορίζει ποια εικόνα θα φορτώσει)
-    steps: [
-      { num: 1, title: 'Cook the pasta', chefId: 1, text: 'Cook your noodles in boiling, salted water, until the pasta is al dente. Drain the pasta and reserve some of the pasta water.' },
-      { num: 2, title: 'Make the sauce', chefId: 2, text: 'While the pasta is cooking, heat olive oil in a pan over medium heat. Add the garlic, and sauté until it starts to turn golden. Add the tomatoes, oregano, salt, and pepper, and cook for 3-4 minutes.' },
-      { num: 3, title: 'Finish the pasta', chefId: 1, text: 'Add the noodles to the sauce, then add pasta water until the sauce is the right consistency. Simmer for 1 minute, then add the spinach, basil, chili flakes, and parmesan.' },
-      { num: 4, title: 'Make the sauce', chefId: 2, text: 'Lower the heat to low, stir until mixed, and remove from the heat. Season to taste, top with parmesan cheese, and enjoy.' }
-    ]
-  };
-
+      recipe: any = null;
   isLiked = false;
 
-  toggleLike() {
+  private route = inject(ActivatedRoute);
+  private recipeService = inject(RecipeService);
+  private cdr = inject(ChangeDetectorRef); 
+
+  ngOnInit() {
+    this.route.paramMap.subscribe(params => {
+      const recipeId = params.get('id');
+      
+      if (recipeId) {
+        this.recipeService.getRecipeById(recipeId).subscribe(data => {
+          this.recipe = data; 
+          
+          this.cdr.detectChanges();
+        });
+      }
+    });
+  }
+
+ toggleLike() {
     this.isLiked = !this.isLiked;
-    this.isLiked ? this.recipe.likes++ : this.recipe.likes--;
+    
+    if (this.recipe) {
+      this.isLiked ? this.recipe.likes++ : this.recipe.likes--;
+      if (this.recipe.id) {
+        this.recipeService.updateLikes(this.recipe.id, this.recipe.likes);
+      }
+    }
   }
 }
