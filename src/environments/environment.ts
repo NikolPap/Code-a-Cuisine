a91dd3e6a6e7618f8eb4b92065486890daa8de3f
Nikoleta Papalazarou <150321195+NikolPap@users.noreply.github.com>
2026-05-04 01:20:46 +0200

    Füge Firebase-Konfiguration für die Entwicklungsumgebung hinzu

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+     production: false,
+
+  firebaseConfig :{
+  apiKey: "AIzaSyA3UHthLXNhGFEy7UJ-rZrK_8Rzk_uoylE",
+  authDomain: "codeacuisine-df68b.firebaseapp.com",
+  projectId: "codeacuisine-df68b",
+  storageBucket: "codeacuisine-df68b.firebasestorage.app",
+  messagingSenderId: "666181454770",
+  appId: "1:666181454770:web:3df2c31828ad94f33a72ab"
+}
+};
