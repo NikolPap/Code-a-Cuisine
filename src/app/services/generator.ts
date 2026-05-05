aeeb48af09057b6ca6af272083d2976bdecdd807
Nikoleta Papalazarou <150321195+NikolPap@users.noreply.github.com>
2026-05-05 23:50:33 +0200

    Füge Generator-Service hinzu: implementiere Rezeptgenerierung über n8n und verwalte Benutzerzutaten

---
+++ src/app/services/generator.ts
@@ -0,0 +1,52 @@
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Ingredient } from '../shared/interfaces';
+import { Observable } from 'rxjs'
+
+@Injectable({
+  providedIn: 'root',
+})
+export class Generator {
+  private http = inject(HttpClient);
+  private n8nWebhookUrl = 'http://localhost:5678/webhook/generate-recipe';
+  private userIngredients: Ingredient[] = [];
+
+
+ setIngredients(ingredients: Ingredient[]) {
+    this.userIngredients = ingredients;
+  }
+  
+  generateRecipesFromN8n(preferences: any): Observable<any> {
+
+    const payload = {
+      action: "generate_recipes",
+      data: {
+        ingredients: this.userIngredients.map(ing => ({
+          name: ing.name,
+          amount: ing.amount,
+          unit: ing.unit
+        })),
+        preferences: {
+          portions: preferences.portions,
+          personsCooking: preferences.persons,
+          timeLimit: preferences.time,
+          cuisineStyle: preferences.cuisine,
+          dietType: preferences.diet
+        }
+      }
+    };
+    
+    return this.http.post(this.n8nWebhookUrl, payload);
+  }
+
+    private generatedRecipes: any[] = []; 
+
+  setGeneratedRecipes(recipes: any[]) {
+    this.generatedRecipes = recipes;
+  }
+
+  getGeneratedRecipes() {
+    return this.generatedRecipes;
+  }
+
+}
