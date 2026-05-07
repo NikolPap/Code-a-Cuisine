78f85bebb0f7fca905f07a57442c2a804e9b4861
Nikoleta Papalazarou <150321195+NikolPap@users.noreply.github.com>
2026-05-07 23:57:27 +0200

    Füge neue Schnittstellen für Benutzerpräferenzen, Modalkonfiguration und vollständige Rezepte hinzu

--- src/app/shared/interfaces.ts
+++ src/app/shared/interfaces.ts
@@ -32,3 +32,43 @@ export interface RecipeListItem {
   tags: string[];
   likes: number;
 }
+
+export interface UserPreferences {
+  portions: number;
+  persons: number;
+  time: string;
+  cuisine: string;
+  diet: string;
+}
+
+export interface ModalConfig {
+  show: boolean;
+  title: string;
+  message: string;
+  btnText: string;
+  btnLink: string;
+}
+
+export interface FullRecipe {
+  id?: string;
+  title: string;
+  time: string;
+  portions: number;
+  chefsCount: number;
+  likes: number;
+  tags: string[];
+  nutrition: {
+    energy: string;
+    protein: string;
+    fat: string;
+    carbs: string;
+  };
+  myIngredients: { amount: string; name: string }[];
+  extraIngredients: { amount: string; name: string }[];
+  steps: { 
+    num: number; 
+    title: string; 
+    chefId: number; 
+    text: string; 
+  }[];
+}
