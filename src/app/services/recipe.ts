67ff9a2559ff0c4b124b2b06a47b1202b6d0ba54
Nikoleta Papalazarou <150321195+NikolPap@users.noreply.github.com>
2026-05-08 14:08:52 +0200

    Verbessere die Typisierung in der RecipeService-Klasse, indem Rückgabetypen und Parameter für Methoden aktualisiert werden.

--- src/app/services/recipe.ts
+++ src/app/services/recipe.ts
@@ -9,38 +9,34 @@ import { FullRecipe } from '../shared/interfaces';
 export class RecipeService {
   private firestore = inject(Firestore);
 
-
-  async saveRecipe(recipeData: any) {
+  async saveRecipe(recipeData: FullRecipe): Promise<string> {
     const recipesCollection = collection(this.firestore, 'recipes');
     const docRef = await addDoc(recipesCollection, recipeData);
     return docRef.id; 
   }
 
-
-  getAllRecipes(): Observable<any[]> {
+  getAllRecipes(): Observable<FullRecipe[]> {
     const recipesCollection = collection(this.firestore, 'recipes');
-    return collectionData(recipesCollection, { idField: 'id' }); 
+    return collectionData(recipesCollection, { idField: 'id' }) as Observable<FullRecipe[]>; 
   }
 
-  
   getRecipeById(id: string): Observable<FullRecipe> {
-  const recipeDoc = doc(this.firestore, `recipes/${id}`);
-  return docData(recipeDoc, { idField: 'id' }) as Observable<FullRecipe>;
-}
+    const recipeDoc = doc(this.firestore, `recipes/${id}`);
+    return docData(recipeDoc, { idField: 'id' }) as Observable<FullRecipe>;
+  }
 
-   async updateLikes(recipeId: string, newLikesCount: number) {
+  async updateLikes(recipeId: string, newLikesCount: number): Promise<void> {
     const recipeDocRef = doc(this.firestore, `recipes/${recipeId}`);
     await updateDoc(recipeDocRef, { likes: newLikesCount });
   }
 
-   async saveIngredientsList(ingredientsArray: string[]) {
+  async saveIngredientsList(ingredientsArray: string[]): Promise<void> {
     const docRef = doc(this.firestore, 'metadata/ingredients');
     await setDoc(docRef, { list: ingredientsArray });
-    console.log("Η λίστα υλικών ανέβηκε!");
   }
 
-  getIngredientsList(): Observable<any> {
+  getIngredientsList(): Observable<{ list: string[] }> {
     const docRef = doc(this.firestore, 'metadata/ingredients');
-    return docData(docRef);
+    return docData(docRef) as Observable<{ list: string[] }>;
   }
 }
