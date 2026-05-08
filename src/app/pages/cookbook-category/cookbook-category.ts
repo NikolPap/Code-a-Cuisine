e99f70a04ab7e792289a23c9612ec1ad0f764ca5
Nikoleta Papalazarou <150321195+NikolPap@users.noreply.github.com>
2026-05-08 14:08:16 +0200

    Aktualisiere den Typ von allMatchedRecipes und recipes in der CookbookCategory-Klasse auf FullRecipe

--- src/app/pages/cookbook-category/cookbook-category.ts
+++ src/app/pages/cookbook-category/cookbook-category.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, inject, ChangeDetectorRef } from '@angular/core';
 import { ActivatedRoute, RouterLink } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { RecipeService } from '../../services/recipe';
-import { RecipeListItem } from '../../shared/interfaces'; 
+import { FullRecipe } from '../../shared/interfaces';
 
 @Component({
   selector: 'app-cookbook-category',
@@ -14,8 +14,8 @@ import { RecipeListItem } from '../../shared/interfaces';
 export class CookbookCategory implements OnInit {
   categoryBanner: string = '';
   currentCategory: string = '';
-  allMatchedRecipes: RecipeListItem[] = []; 
-  recipes: RecipeListItem[] = []; 
+  allMatchedRecipes: FullRecipe[] = []; 
+  recipes: FullRecipe[] = []; 
   currentPage: number = 1;
   itemsPerPage: number = 15; 
   totalPages: number = 1;
@@ -26,23 +26,27 @@ export class CookbookCategory implements OnInit {
   private cdr = inject(ChangeDetectorRef);
 
   ngOnInit() {
-    this.route.paramMap.subscribe(params => {
-      this.currentCategory = params.get('category') || '';
-      
-      if (this.currentCategory) {
-        this.categoryBanner = `${this.currentCategory}-recipe.svg`;
-        this.recipeService.getAllRecipes().subscribe((allRecipes: RecipeListItem[]) => {
-          this.allMatchedRecipes = allRecipes.filter(recipe => {
-            if (!recipe.tags || !Array.isArray(recipe.tags)) return false;
-            return recipe.tags.some((tag: string) => tag.toLowerCase() === this.currentCategory.toLowerCase());
-          });
-          this.totalPages = Math.ceil(this.allMatchedRecipes.length / this.itemsPerPage) || 1;
-          this.pageNumbers = Array.from({length: this.totalPages}, (_, i) => i + 1);
-          this.goToPage(1);
+  this.route.paramMap.subscribe(params => {
+    this.currentCategory = params.get('category') || '';
+    
+    if (this.currentCategory) {
+      this.categoryBanner = `${this.currentCategory}-recipe.svg`;
+      this.recipeService.getAllRecipes().subscribe((allRecipes: FullRecipe[]) => {
+        
+        this.allMatchedRecipes = allRecipes.filter(recipe => {
+          if (!recipe.tags || !Array.isArray(recipe.tags)) return false;
+          return recipe.tags.some((tag: string) => 
+            tag.toLowerCase() === this.currentCategory.toLowerCase()
+          );
         });
-      }
-    });
-  }
+
+        this.totalPages = Math.ceil(this.allMatchedRecipes.length / this.itemsPerPage) || 1;
+        this.pageNumbers = Array.from({length: this.totalPages}, (_, i) => i + 1);
+        this.goToPage(1);
+      });
+    }
+  });
+}
 
   goToPage(page: number) {
     if (page >= 1 && page <= this.totalPages) {
